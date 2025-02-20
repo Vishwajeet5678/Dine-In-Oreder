74fbc9a94a6277e76db4566e3d4a6f07dce80f30
Vishwajeet5678 <vishwajithbm99@gmail.com>
2025-02-20 08:54:48 +0530

    Project initiate

---
+++ src/main/java/com/example/Dine_In_Order/DineInOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dine_In_Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineInOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dine_In_Order/DineInOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dine_In_Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineInOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
