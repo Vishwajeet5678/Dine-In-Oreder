16c4218be7f197c71c8bd399096a062452097f6e
Vishwajeet5678 <vishwajithbm99@gmail.com>
2025-02-27 14:31:26 +0530

    Created the ResponceStructure Class and added AllArgsConstructor

--- src/main/java/com/example/dine_in_order/controller/UserController.java
+++ src/main/java/com/example/dine_in_order/controller/UserController.java
@@ -1,4 +1,28 @@
-package com.example.Dine_In_Order.controller;
+package com.example.dine_in_order.controller;
 
+import com.example.dine_in_order.model.User;
+import com.example.dine_in_order.service.UserService;
+import com.example.dine_in_order.util.ResponseStructure;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class UserController {
+
+    private  final UserService userService;
+    @PostMapping("/register")
+    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user)
+    {
+        user=userService.registerUser(user);
+        ResponseStructure<User> structure=new ResponseStructure<User>();
+        structure.setData(user);
+        structure.setStatus(HttpStatus.CREATED.value());
+        structure.setMessage("User Created");
+        return new ResponseEntity<ResponseStructure<User>>(structure,HttpStatus.CREATED);
+    }
 }
