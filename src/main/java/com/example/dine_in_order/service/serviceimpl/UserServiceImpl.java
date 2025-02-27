ce414c1530c7f1be174b6b98de2d416db47f5e07
Vishwajeet5678 <vishwajithbm99@gmail.com>
2025-02-27 17:11:35 +0530

    Created two methods createUserByRole and mapToNewUser

--- src/main/java/com/example/dine_in_order/service/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/dine_in_order/service/serviceimpl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.dine_in_order.service.serviceimpl;
 
+import com.example.dine_in_order.enums.UserRole;
+import com.example.dine_in_order.model.Admin;
+import com.example.dine_in_order.model.Staff;
 import com.example.dine_in_order.model.User;
 import com.example.dine_in_order.repository.UserRepository;
 import com.example.dine_in_order.service.UserService;
@@ -14,6 +17,28 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     @Override
     public User registerUser(User user) {
-        return userRepository.save(user);
+       User child=this.createUserByRole(user.getRole());
+       this.mapToNewUser(user,child);
+        return userRepository.save(child);
     }
+
+    private  User createUserByRole(UserRole role) {
+        User user;
+        switch (role)
+        {
+            case ADMIN -> user=new Admin();
+            case STAFF -> user=new Staff();
+            default -> throw new RuntimeException("Invalid user");
+        }
+        return user;
+    }
+    private void mapToNewUser(User user,User user2){
+        user2.setUserName(user.getUserName());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setPhno(user.getPhno());
+        user2.setRole(user.getRole());
+
+    }
+
 }
