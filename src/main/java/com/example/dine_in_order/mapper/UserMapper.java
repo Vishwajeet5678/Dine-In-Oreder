7142d370503b5ee230ee9e3ad150ba00b51f144e
Vishwajeet5678 <vishwajithbm99@gmail.com>
2025-02-28 17:40:58 +0530

    added the plugins of  mapperstruct

--- src/main/java/com/example/dine_in_order/mapper/UserMapper.java
+++ src/main/java/com/example/dine_in_order/mapper/UserMapper.java
@@ -4,30 +4,13 @@ import com.example.dine_in_order.dto.request.RegistrationRequest;
 import com.example.dine_in_order.dto.request.UserRequest;
 import com.example.dine_in_order.dto.response.UserResponse;
 import com.example.dine_in_order.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.springframework.stereotype.Component;
 
-@Component
-public class UserMapper {
-    public void mapToNewUserRequest(UserRequest user, User user2) {
-        user2.setUserName(user.getUserName());
-        user2.setEmail(user.getEmail());
-        user2.setPhno(user.getPhno());
-    }
-    public void mapToUser(RegistrationRequest user, User user2){
-        user2.setUserName(user.getUserName());
-        user2.setEmail(user.getEmail());
-        user2.setPassword(user.getPassword());
-        user2.setPhno(user.getPhno());
-        user2.setRole(user.getRole());
-
-    }
-    public UserResponse mapToUserResponse(User user) {
-        return UserResponse.builder()
-                .userId(user.getUserId())
-                .userName(user.getUserName())
-                .createdAt(user.getCreateAt())
-                .role(user.getRole())
-                .lastModifiedAt(user.getLastModifiedAt())
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    void mapToNewUserRequest(UserRequest user,@MappingTarget User user2);
+     void mapToUser(RegistrationRequest user,@MappingTarget User user2);
+     UserResponse mapToUserResponse(User user) ;
 }
