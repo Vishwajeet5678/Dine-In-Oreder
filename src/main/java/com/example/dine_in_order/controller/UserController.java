0c65cc34d7d7bab467264ba91fae28f975c3dd18
Vishwajeet5678 <vishwajithbm99@gmail.com>
2025-03-04 16:19:45 +0530

    created the base url

--- src/main/java/com/example/dine_in_order/controller/UserController.java
+++ src/main/java/com/example/dine_in_order/controller/UserController.java
@@ -5,8 +5,15 @@ import com.example.dine_in_order.dto.request.UserRequest;
 import com.example.dine_in_order.dto.response.UserResponse;
 import com.example.dine_in_order.model.User;
 import com.example.dine_in_order.service.UserService;
+import com.example.dine_in_order.util.FieldErrorResponse;
 import com.example.dine_in_order.util.ResponseBuilder;
 import com.example.dine_in_order.util.ResponseStructure;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,23 +24,55 @@ import static java.util.stream.DoubleStream.builder;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1")
+@Tag(name = "User Controller",description = "Collection API Endpoints dealing with the user data")
 public class UserController {
 
     private  final UserService userService;
     @PostMapping("/register")
+    @Operation(description = """
+            Collection API Endpoint is used to register user.
+            The endpoint requires the user to select one of the specified role along with the other details.
+            """,
+    responses = {
+            @ApiResponse(responseCode = "201",description = "User Created"),
+            @ApiResponse(responseCode = "400",description = "Invalid input",content = {
+                    @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+            })
+    })
     public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody @Valid RegistrationRequest user)
     {
         UserResponse response=userService.registerUser(user);
         return ResponseBuilder.sucess(HttpStatus.CREATED,"User Created",response);
     }
     @GetMapping("/{userId}")
+    @Operation(description = """
+            Collection API Endpoint is used to get the user.
+            The endpoint requires the user to enter the userid based on the id the user details will fetch.
+            """,
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "User Found"),
+                    @ApiResponse(responseCode = "400",description = "Invalid input",content = {
+                            @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+                    })
+            })
     public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable("userId") long userId)
     {
         UserResponse user=userService.findUserById(userId);
         return ResponseBuilder.sucess(HttpStatus.OK,"User Found",user);
     }
     @PutMapping("/{userId}")
-    public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@PathVariable("userId")long userId,@RequestBody UserRequest user)
+    @Operation(description = """
+            Collection API Endpoint is used to register user.
+            The endpoint requires the user to select one of the specified role along with the other details.
+            """,
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "User Updated"),
+                    @ApiResponse(responseCode = "400",description = "Invalid input",content = {
+                            @Content(schema = @Schema(implementation = FieldErrorResponse.class))
+                    })
+            })
+    public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@PathVariable("userId")long userId,@RequestBody @Valid UserRequest user)
     {
          UserResponse response=userService.updateUserById(userId,user);
         return ResponseBuilder.sucess(HttpStatus.OK,"User Updated",response);
--- src/main/java/com/example/dine_in_order/dto/request/RegistrationRequest.java
+++ src/main/java/com/example/dine_in_order/dto/request/RegistrationRequest.java
@@ -1,10 +1,7 @@
 package com.example.dine_in_order.dto.request;
 
 import com.example.dine_in_order.enums.UserRole;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,8 +14,15 @@ public class RegistrationRequest {
     private String userName;
     @NotEmpty(message = "Email can not be empty")
     @NotBlank(message = "Email can not be blank")
+    @Email(regexp = "^[a-zA-Z0-9._%+-]+@gmail\\.com$",
+            message = "The email should begin with letters, numbers, or allowed special characters (., _, %, +, -) and must end with \"@gmail.com")
     private String email;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,12}$",
+            message = "The password must be between 8 to 12 characters long and must include at least one uppercase letter, one lowercase letter, one number, and one special character.")
+    @NotEmpty(message = "Password can not be empty")
+    @NotBlank(message = "Password can not be blank")
     private String password;
+    @Pattern(regexp = "^(\\+91[\\s-]?|91[\\s-]?)?[6-9]\\d{9}$",message = "The mobile number should be exactly 10 digits long and must start with a digit between 6 and 9")
     private String phno;
     private UserRole role;
 }
